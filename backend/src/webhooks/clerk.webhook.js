661dd9aee8ed4b7e982eadd4f2c7c2d5f1751633
Diego Juárez Vázquez <diegojve27@gmail.com>
2026-08-14 19:02:45 -0600

    fix formatting in clerk webhook file by correcting import statements

--- backend/src/webhooks/clerk.webhook.js
+++ backend/src/webhooks/clerk.webhook.js
@@ -1,7 +1,6 @@
-import express from "express"
-import User from "../models/user.model.js"
-import { verifyWebhook } from "@clerk/backend/webhooks"
-import e from "express";
+import express from "express";
+import User from "../models/user.model.js";
+import { verifyWebhook } from "@clerk/backend/webhooks";
 
 const router = express.Router();
 
